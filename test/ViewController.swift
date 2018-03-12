d31f34679220ddeb7ad46d394ea6b35cea992738
Vikas <vikas.calica@gmail.com>
2018-03-12 19:34:49 +0530

    test from tree

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -20,15 +20,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-//    override func viewDidLayoutSubviews() {
-//   
-//        if btn.frame.size.width < btn.frame.size.height
-//        {
-//            btn.frame.size.width = btn.frame.size.height
-//            btn.layer.cornerRadius = btn.frame.size.height/2
-//            btn.clipsToBounds = true
-//        }
-//    }
+
    
 }
 
